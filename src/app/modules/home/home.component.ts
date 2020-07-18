74816328d42e48b2208bedaa568d5b8773875bea
unknown <cameron.erwin@hotmail.com>
2020-07-17 21:41:17 -0400

    Fixed Rellax header image

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as Rellax from 'rellax';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+
+  public rellaxHeader: Rellax;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.rellaxHeader = new Rellax('.rellax-header');
   }
 
 }
